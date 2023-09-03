ef8738f043328f3e9c7b149e8ac9c8b386430ecf
WaveShapePlay <WaveShapePlay@gmail.com>
2023-09-03 07:51:57 -0400

    Fixed arduino logic

--- rowingMachine.ino
+++ rowingMachine.ino
@@ -1,6 +1,7 @@
 int analogPin = 3;     
 int data = 0;  
-int stopFlag = 0;       
+int stopFlag = 0;
+int threshhold = 800;       
 char userInput;
 unsigned long startTime = 0;
 unsigned long endTime = 0;
@@ -13,37 +14,39 @@ void setup(){
 void loop(){
 
 if(Serial.available()> 0){ 
-  
   userInput = Serial.read();               // read user input
-
   if(userInput == 'o'){
   data = analogRead(analogPin);    // read the input pin
     
-    if(data < 800){  // 1000 for reed 800 for testing
+    if(data < threshhold){  // 1000 for reed 800 for testing
       startTime = millis();
       Serial.println(data);
-
-      while(data <800 && stopFlag == 0){
-        Serial.println(stopFlag);
+      
+      while(stopFlag == 0){
+        data = analogRead(analogPin);    // read the input pin
+        
         if(Serial.available() > 0){ 
           userInput = Serial.read(); 
+          
           if(userInput == 'x'){
             stopFlag = 1;
-          }
           
-        }
+          } // if user input is 'x' to stop
+        } // check to see if there is user input
         
-        data = analogRead(analogPin);
-        Serial.println(data);
+        if(data < threshhold){
+          Serial.println(data);
         
-      } // while data < 1000
+        } // print if data < threshold
+      } // while stopFlag == 0
+      
       stopFlag = 0;
       endTime = millis();
       Serial.print("Total Time: ");
       Serial.println(endTime - startTime);
   
     } // first if data < 1000
-  } // if user input equal to 'e'
+  } // if user input equal to 'o'
 } //if serial.available            
        
 } // Void Loop
